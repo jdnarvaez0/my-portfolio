0acefa6fb993136ac2c534a7a4055bf6159ebbc8
Juan Narvaez <juand.ns07@gmail.com>
2023-10-03 15:58:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<div align="center">
+  <img alt="Astro Theme Cactus logo" src="" width="70" />
+</div>
+<h1 align="center">
+  Portfolio
+</h1>
+
+This is my portfolio to share my thoughts and my work as a developer. It's built with Astro, and I've customized a [template](https://github.com/chrismwilliams/astro-theme-cactus/tree/main) to my liking, as well as added some enhancements.
+
+## Commands
+
+Replace pnpm with your choice of npm / yarn
+
+| Command          | Action                                                         |
+| :--------------- | :------------------------------------------------------------- |
+| `pnpm install`   | Installs dependencies                                          |
+| `pnpm dev`       | Starts local dev server at `localhost:3000`                    |
+| `pnpm build`     | Build your production site to `./dist/`                        |
+| `pnpm postbuild` | Pagefind script to build the static search of your blog posts  |
+| `pnpm preview`   | Preview your build locally, before deploying                   |
+| `pnpm sync`      | Generate types based on your config in `src/content/config.ts` |
+
+## License
+
+MIT
---
+++ src/site.config.ts
@@ -0,0 +1,43 @@
+import type { SiteConfig } from "@/data/types";
+
+export const siteConfig: SiteConfig = {
+	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+	author: "Juan Narváez",
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	title: "Juan Narváez",
+	// Meta property used as the default description meta property
+	description: "An opinionated starter theme for Astro",
+	// HTML lang property, found in src/layouts/Base.astro L:18
+	lang: "en-GB",
+	// Meta property, found in src/components/BaseHead.astro L:42
+	ogLocale: "en_GB",
+	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+	date: {
+		locale: "en-GB",
+		options: {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+		},
+	},
+};
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "Projects",
+		path: "/works/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+	{
+		title: "CV",
+		path: "/cv/",
+	},
+];
